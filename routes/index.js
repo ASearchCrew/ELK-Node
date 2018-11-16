0f00cdf0a1faffe1b40790d05e4d7ae53083ad26
KiWooyoung <ueong777@gmail.com>
2018-11-16 11:35:17 +0900

    ENH : Add Log

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+  console.log("이건 로그야 filebeat 야");
+
   return res.status(200).json({
       Test : "B",
       Sample : "C"
